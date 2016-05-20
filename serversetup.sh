7830d86c6c0b5bc66a6c824b5f4275d75ade9995
Marcus Guttenplan <marcusxhim@gmail.com>
2016-05-20 15:35:08 -0700

    fix http error
    one letter, misplaced

--- serversetup.sh
+++ serversetup.sh
@@ -294,7 +294,7 @@ iptables -A OUTPUT -o lo -j ACCEPT
 iptables -A INPUT -i eth0 -p tcp --dport 80 -m state --state NEW,ESTABLISHED -j ACCEPT
 iptables -A OUTPUT -o eth0 -p tcp --sport 80 -m state --state ESTABLISHED -j ACCEPT
 
-# Allow outgoing HTTPS
+# Allow outgoing HTTP
 iptables -A OUTPUT -o eth0 -p tcp --dport 80 -m state --state NEW,ESTABLISHED -j ACCEPT
 iptables -A INPUT -i eth0 -p tcp --sport 80 -m state --state ESTABLISHED -j ACCEPT
 
